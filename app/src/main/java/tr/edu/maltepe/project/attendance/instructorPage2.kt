ebc3779efb4f3dd7f2bf9f76f9bd98a93e5b32b4
İrem Kutnay <iremkutnay@gmail.com>
2022-05-11 22:58:04 +0300

    updated

--- app/src/main/java/tr/edu/maltepe/project/attendance/instructorPage2.kt
+++ app/src/main/java/tr/edu/maltepe/project/attendance/instructorPage2.kt
@@ -2,10 +2,33 @@ package tr.edu.maltepe.project.attendance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class instructorPage2 : AppCompatActivity() {
+
+    val lessonhour = arrayOf("0","1","2","3","4","5","6","7","8","9")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructor_page2)
+
+        val spinnerLessonhour = findViewById<Spinner>(R.id.spinnerLessonhour)
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,lessonhour)
+
+        spinnerLessonhour.adapter = arrayAdapter
+        spinnerLessonhour.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
 }
